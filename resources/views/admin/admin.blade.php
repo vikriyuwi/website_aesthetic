7dd221446bdd48e16a27846fa9007a381a66a504
vikriyuwi <latifulfikri@outlook.com>
2024-12-16 16:58:30 +0700

    navbar — general & admin fix

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -111,7 +111,7 @@
                     <span class="text-gray-800 font-semibold">Admin</span>
                 </button>
                 <div id="profileDropdown" class="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg hidden">
-                    <a href="#" class="block px-4 py-2 text-red-600 hover:bg-indigo-50">Logout</a>
+                    <a href="{{ route('logout') }}" class="block px-4 py-2 text-red-600 hover:bg-indigo-50">Logout</a>
                 </div>
             </div>
         </header>
--- resources/views/layouts/navbar-login.blade.php
+++ resources/views/layouts/navbar-login.blade.php
@@ -44,20 +44,12 @@
                         <img src="{{ Auth::user()->Buyer->PROFILE_IMAGE_URL != null ? asset(Auth::user()->Buyer->PROFILE_IMAGE_URL) : "https://placehold.co/100x100"}}" class="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center text-xl font-bold text-gray-500">
                         <div class="ml-4">
                                 <div class="font-semibold text-gray-800">{{ Auth::user()->Buyer->FULLNAME }}</div>
-                                @if (Auth::user()->USER_LEVEL == 1)
-                                <a href="{{ route('buyer.showProfile') }}" class="text-sm text-indigo-600 hover">View profile</a>
-                                @elseif(Auth::user()->USER_LEVEL == 2)
-                                @php
-                                $id = Auth::user()->artist ? Auth::user()->artist->ARTIST_ID : null;
-                                @endphp
-                                <a href="{{ route('artist.show', $id) }}" class="text-sm text-indigo-600 hover">View profile</a>
-                                @endif
                         </div>
                 </div>
 
                 <!-- My Collection Section -->
                 <div class="py-4 px-4 border-b border-gray-200">
-                <div class="text-sm text-gray-500 mb-5">My Collection</div>
+                {{-- <div class="text-sm text-gray-500 mb-5"></div> --}}
                 @if(Auth::user()->USER_LEVEL == 1)
                 <div class="flex items-center mb-1 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-black mr-3">
@@ -66,18 +58,39 @@
                         <a href="{{ route('join-artist') }}" class="text-gray-800 hover">Join Artist</a>
                 </div>
                 @endif
+                @if (Auth::user()->Buyer != null)
                 <div class="flex items-center mb-1 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-black mr-3">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
                         </svg>
-                        <a href="#" class="text-gray-800 hover">Artists</a>
+                        <a href="{{ route('buyer.showProfile') }}" class="text-gray-800 hover">Buyer Profile</a>
                 </div>
+                @endif
+                @if (Auth::user()->Artist != null)
+                <div class="flex items-center mb-1 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-black mr-3">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                        </svg>
+                        <a href="{{ route('artist.show', Auth::user()->artist->ARTIST_ID) }}" class="text-gray-800 hover">Buyer Profile</a>
+                </div>
+                @endif
+                @if (Auth::user()->USER_LEVEL == 3)
+                <div class="flex items-center mb-1 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-black mr-3">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M3 9.75V5.25c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3 9.75ZM3 18.75v-4.5c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3 18.75ZM14.25 5.25c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125V5.25ZM14.25 18.75v-4.5c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125Z" />
+                        </svg>
+                              
+                        <a href="{{ route('admin.dashboard') }}" class="text-gray-800 hover">Admin Dashboard</a>
+                </div>
+                @endif
+                @if (Auth::user()->Artist != null)
                 <div class="flex items-center mb-1 hover:bg-gray-100 p-2 rounded-lg cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-black mr-3">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
                         </svg>
                         <a href="{{ route('insight-artist') }}" class="text-gray-800 hover">Insights</a>
                 </div>
+                @endif
         </div>
 
                         <!-- Favorites Section -->
